24a90ec4d83721e2b3e5375502328d48b9a57628
Angeleena Livingston D <angeleenalivingston@gmail.com>
2026-02-11 06:50:15 +0530

    django

--- app/views.py
+++ app/views.py
@@ -4,12 +4,12 @@ from django.shortcuts import render
 
 def app(request):
     my_objects = [
-        {'name': 'John', 'Job_title': 'Manager', 'salary': 84000,'employee_type':'Part time'},
-        {'name': 'Jane', 'Job_title': 'Employee', 'salary': 25000,'employee_type':'Full time'},
-        {'name': 'Bob', 'Job_title': 'HR Manager', 'salary':48000,'employee_type':'Full time' },
-        {'name': 'Ann', 'Job_title': 'Programmer', 'salary': 60000,'employee_type':'Full time'},
-        {'name': 'George', 'Job_title': 'Assistant', 'salary':25000,'employee_type':'Part time'},
-        {'name': 'Seena', 'Job_title': 'Employee', 'salary': 25000,'employee_type':'Part time'}
+        {'name': 'John', 'Job_title': 'Manager', 'salary': 84000,'is_full_time':'Part time'},
+        {'name': 'Jane', 'Job_title': 'Employee', 'salary': 25000,'is_full_time':'Full time'},
+        {'name': 'Bob', 'Job_title': 'HR Manager', 'salary':48000,'is_full_time':'Full time' },
+        {'name': 'Ann', 'Job_title': 'Programmer', 'salary': 60000,'is_full_time':'Full time'},
+        {'name': 'George', 'Job_title': 'Assistant', 'salary':25000,'is_full_time':'Part time'},
+        {'name': 'Seena', 'Job_title': 'Employee', 'salary': 25000,'is_full_time':'Part time'}
         
     ]
     context = {'my_objects': my_objects}
